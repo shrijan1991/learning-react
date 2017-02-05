3bc4a28f0636cb591ac145172d6a687c974f1052
Shrijan Karmacharya <shrijan.2013@gmail.com>
2017-02-05 15:52:42 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const { render } = require('react-dom');
+import Board from './src/Board';
+
+render(<Board />, document.getElementById('board'));
---
+++ src/Board.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+  render() {
+    return (  
+      <div>
+        <h1>Ludo Board Game</h1>
+      </div>
+    )
+  }
+}
+
+export default Board;
